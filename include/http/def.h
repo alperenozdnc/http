b7076feb74feb139ede160496bd061b767e8446a
alperenozdnc <alperen@ozdinc.org>
2025-12-30 02:21:31 +0300

    chore(server): define some macros for future use

---
+++ include/http/def.h
@@ -0,0 +1,10 @@
+/*
+ * miscellaneous definitions used throughout the program.
+ */
+
+#pragma once
+
+#define HTTP_PREFIX "[http] "
+#define HTTP_REQUEST_PREFIX "[http/req] "
+#define HTTP_RESPONSE_PREFIX "[http/res] "
+#define HTTP_MAX_REQUEST_SIZE 2048
