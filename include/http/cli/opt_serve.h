9078354aed5b4cb246154ead1044766c93593fc2
alperenozdnc <alperen@ozdinc.org>
2025-12-30 21:02:35 +0300

    chore: add code comments for uncommented functions

--- include/http/cli/opt_serve.h
+++ include/http/cli/opt_serve.h
@@ -4,4 +4,7 @@
 
 ret_e opt_serve_body(CLI_IGNORE_CTX, char *str);
 
+/*
+ * the main option used to start a local server on a specified port.
+ */
 void init_opt_serve(cli_s *cli);
--- include/http/utils/print_http_header.h
+++ include/http/utils/print_http_header.h
@@ -2,4 +2,7 @@
 
 #include <stddef.h>
 
+/*
+ * prints the first line of an http request/response.
+ */
 size_t print_http_header(char *req);
