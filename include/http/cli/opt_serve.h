9c6d8cecce95d3106164df9e52cddc495e6cd878
alperenozdnc <alperen@ozdinc.org>
2026-01-02 15:45:34 +0300

    chore(cli): include context in the serve option

--- include/http/cli/opt_serve.h
+++ include/http/cli/opt_serve.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <http/ctx.h>
+
 #include <ezcli.h>
 
 ret_e opt_serve_body(CLI_IGNORE_CTX, char *str);
@@ -7,4 +9,4 @@ ret_e opt_serve_body(CLI_IGNORE_CTX, char *str);
 /*
  * the main option used to start a local server on a specified port.
  */
-void init_opt_serve(cli_s *cli);
+void init_opt_serve(cli_s *cli, ctx_s *ctx);
