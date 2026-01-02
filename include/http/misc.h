8916dc21b4f8139a03f144fb565ef0a28c520145
alperenozdnc <alperen@ozdinc.org>
2026-01-02 15:47:18 +0300

    feat(misc): `ASSERT_SERVE_MODE_ENABLED` and `SERVE_HTTP` macros

--- include/http/misc.h
+++ include/http/misc.h
@@ -28,3 +28,33 @@
         print_http_header(buf);                                                \
         printf("\n");                                                          \
     } while (0);
+
+/*
+ * helper for asserting that the 'serve' option must be called at least once
+ * when running the program.
+ */
+#define ASSERT_SERVE_MODE_ENABLED(ctx, want_input)                             \
+    do {                                                                       \
+        if (is_last_opt(ctx, want_input) && !ctx->serve_mode) {                \
+            cliprint(CLI_ERROR, HTTP_PREFIX,                                   \
+                     "you need to include 'serve [port]' when you run this "   \
+                     "program.");                                              \
+                                                                               \
+            return RET_FAIL;                                                   \
+        }                                                                      \
+    } while (0);
+
+/*
+ * helper for serving http on a specific port. this macro exists because the
+ * 'serve' option isn't the only entrypoint. */
+#define SERVE_HTTP(port)                                                       \
+    do {                                                                       \
+        cliprint(CLI_HINT, HTTP_PREFIX,                                        \
+                 "serving on port %d | <http://localhost:%d>\n", port, port);  \
+                                                                               \
+        if (!server(htons(port))) {                                            \
+            cliprint(CLI_ERROR, HTTP_PREFIX, "error starting server.");        \
+                                                                               \
+            return RET_FAIL;                                                   \
+        }                                                                      \
+    } while (0);
