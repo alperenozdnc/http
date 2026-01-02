97d3ad5465262eff799d38f76191ef8ca44eb839
alperenozdnc <alperen@ozdinc.org>
2026-01-02 15:48:18 +0300

    feat(cli/serve): this option can now handle not being the last option
    because of the way ezcli way internally designed, there is no
    pre-parsing, all tokens are read and executed, left to right, that means
    that if somebody doesn't fit their usage to the form 'cfg1 cfg2 serve
    port' then it would run the server without actually applying the
    configuration.
    
    i fixed this problem by introducing a 'serve mode' in my context struct,
    whenever one of the available options are the last option in argv, they
    run the server regardless of if they're even 'serve' or not, although,
    the inclusion of 'serve' at least once is forced.

--- cli/opts/serve.c
+++ cli/opts/serve.c
@@ -1,4 +1,7 @@
+#include <http/cli/is_last_opt.h>
 #include <http/cli/opt_serve.h>
+
+#include <http/ctx.h>
 #include <http/def.h>
 #include <http/misc.h>
 #include <http/server.h>
@@ -6,30 +9,35 @@
 #include <netinet/in.h>
 
 #include <ezcli.h>
+
 #include <stdlib.h>
 
-ret_e opt_serve_body(CLI_IGNORE_CTX, char *tok) {
-    CLEAR_CONSOLE();
+#define DOES_OPTION_WANT_INPUT true
+
+ret_e opt_serve_body(void *_ctx, char *tok) {
+    ctx_s *ctx = _ctx;
 
     in_port_t port = atoi(tok);
 
-    cliprint(CLI_HINT, HTTP_PREFIX,
-             "serving on port %d | <http://localhost:%d>\n", port, port);
+    if (!is_last_opt(ctx, DOES_OPTION_WANT_INPUT)) {
+        ctx->serve_mode = true;
+        ctx->port = port;
 
-    if (!server(htons(port))) {
-        cliprint(CLI_ERROR, HTTP_PREFIX, "error starting server.");
-
-        return RET_FAIL;
+        return RET_NORMAL;
     }
+
+    CLEAR_CONSOLE();
+    SERVE_HTTP(port);
 
     return RET_NORMAL;
 }
 
-void init_opt_serve(cli_s *cli) {
+void init_opt_serve(cli_s *cli, ctx_s *ctx) {
     opt_s opt_default = {.aliases = CLI_ALIASES("serve", "server", "--serve"),
                          .body = opt_serve_body,
                          .desc = "starts an http server on the provided port.",
-                         .want_input = true};
+                         .want_input = true,
+                         .ctx = ctx};
 
     allocopt(cli, &opt_default);
 }
