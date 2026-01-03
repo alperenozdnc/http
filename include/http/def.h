90411dae9659f58d99d3d3af32af2b424ce36b41
alperenozdnc <alperen@ozdinc.org>
2026-01-03 20:13:16 +0300

    chore(server): add some useful definitions

--- include/http/def.h
+++ include/http/def.h
@@ -8,3 +8,19 @@
 #define HTTP_REQUEST_PREFIX "[http/req] "
 #define HTTP_RESPONSE_PREFIX "[http/res] "
 #define HTTP_MAX_REQUEST_SIZE 2048
+
+#define OK_STATUS "HTTP/1.0 200 OK"
+#define NOT_FOUND_STATUS "HTTP/1.0 404 NOT FOUND"
+
+#define HTML_EXTENSION ".html"
+
+#define NOT_FOUND_PAGE "404" HTML_EXTENSION
+#define NOT_FOUND_DEFAULT_PAGE "assume_this_is_default_404"
+
+#define CONTENT_TYPE_PREFIX "Content-Type: "
+#define CONTENT_LENGTH_PREFIX "Content-Length: "
+
+#define CRLF "\r\n"
+#define CRLF_LEN 2 // strlen("\r\n")
+
+#define INDEX_PAGE "index" HTML_EXTENSION
